03bfaf18ca2193862e1e9398bc813ad779c433d5
lexmin0412 <zhangle_dev@outlook.com>
2025-12-12 00:14:09 +0800

    chore(config): 设置 turbopack 根目录，以防止 Next 自动寻找根目录时找错

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: false,
+  // output: 'standalone', // 暂不开启，如果体积实在太大再开
+  turbopack: {
+    root: path.resolve(__dirname),
+  }
 };
 
 export default nextConfig;
